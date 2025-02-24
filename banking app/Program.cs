95d5e86f14394cc7f6c89f2448e4886f7e5da045
HP <HP@DESKTOP-JTFC2LN>
2025-02-24 09:11:43 -0800

    Add project files.

---
+++ banking app/Program.cs
@@ -0,0 +1,22 @@
+﻿//simple banking app
+
+
+
+Console.Write("Name:");
+ string name = Console.ReadLine();
+Console.Write("Password:");
+string lastname = Console.ReadLine();
+Console.WriteLine("Hello {0} welcome to theBulb MicrofinanceBank. Here's your account no:10992783938", name);
+//Console.WriteLine("Hello " +  name  + " welcome to theBulb MicrofinanceBank. Here's your account no:10992783938");
+
+//2.if name and password is correct,view account balance
+bool isName = true, isPassword = true;
+if (isName && isPassword);
+Console.WriteLine("Account Balance:$25,000");
+
+bool isDetailsCorrect = true;
+bool isAmount2000 = true;
+if (isDetailsCorrect== isAmount2000) ;
+Console.WriteLine("Successful");
+
+
