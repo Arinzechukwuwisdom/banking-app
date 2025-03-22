943ef54a979c49dad2750dd9a36f17234595da5c
Arinzechukwu <wisdomsure5@gmail.com>
2025-03-22 10:20:07 -0700

    Update Program.cs

--- banking app/Program.cs
+++ banking app/Program.cs
@@ -1,4 +1,24 @@
-﻿//simple banking app
+Using System;﻿
+namespace BankingApp{
+class Program{
+ public class User{
+  public string Name{get;set;}
+  public string Email{get;set;}
+  public string PhoneNumber{get;set}
+  public DateTime DateOfBirth{get;set;}
+  public User( string name, string email , string phoneNumber, DateTime dateOfBirth) {
+   
+   Name = name;
+Email = email;
+PhoneNumber = phoneNumber;
+DateOfBirth = dateOfBirth;
+  }
+ }
+ static void Main(){
+  
+ }
+}
+}
 
 
 
