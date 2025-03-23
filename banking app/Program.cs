dc29dd72a727ec01fe4872ec0af6cc4d4f51ff65
Arinzechukwu <wisdomsure5@gmail.com>
2025-03-23 16:10:53 -0700

    Update Program.cs

--- banking app/Program.cs
+++ banking app/Program.cs
@@ -6,15 +6,45 @@ class Program{
   public string Email{get;set;}
   public string PhoneNumber{get;set}
   public DateTime DateOfBirth{get;set;}
-  public User( string name, string email , string phoneNumber, DateTime dateOfBirth) {
+  public string Password { get; }
+  public User( string name, string email , string phoneNumber, DateTime dateOfBirth,string password) {
    
-   Name = name;
+Name = name;
 Email = email;
 PhoneNumber = phoneNumber;
 DateOfBirth = dateOfBirth;
+Password=password;
+   
   }
  }
- static void Main(){
+ public class BankAccount {
+     public string AccountNumber { get; }// the reason i used get only is bc i want to generate the account number automatically and I dont want it to be changed.
+     public decimal Balance { get; private set; }
+     public string AccountHolder { get; }
+
+     public BankAccount(string accountnumber, decimal balance, string accountHolder)
+     {
+
+         AccountNumber = accountnumber;
+         Balance = balance;
+         AccountHolder = accountHolder;
+     }
+  }
+   public class Transaction {
+
+public string TransactionID { get; } = Guid.NewGuid().ToString();
+    public decimal Amount { get; }
+    public string FromAccount{ get; }
+    public string ToAccount { get; }
+    public DateTime Date { get; } = DateTime.Now;
+
+    public Transaction(string transactionID, decimal amount, string fromAccount, string toAccount, DateTime dateNow) {
+    Amount = amount;
+    FromAccount = fromAccount;
+    ToAccount = toAccount;
+    Date = dateNow;
+}
+ static void Main()
   
  }
 }
@@ -22,14 +52,14 @@ DateOfBirth = dateOfBirth;
 
 
 
-Console.Write("Name:");
+Console.Write("Enter Your Name:");
  string name = Console.ReadLine();
 Console.Write("Password:");
 string lastname = Console.ReadLine();
 Console.WriteLine("Hello {0} welcome to theBulb MicrofinanceBank. Here's your account no:10992783938", name);
 //Console.WriteLine("Hello " +  name  + " welcome to theBulb MicrofinanceBank. Here's your account no:10992783938");
 
-//2.if name and password is correct,view account balance
+if (name and password is correct,view account balance
 bool isName = true, isPassword = true;
 if (isName && isPassword);
 Console.WriteLine("Account Balance:$25,000");
